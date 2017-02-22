f363a8be7be8c327bb95eac4bbe1ac3016717bc3
Chen Alon <chen@bwebi.co.il>
2017-02-22 18:33:53 +0000

    Apply fixes from StyleCI

--- src/Bwebi/SeoRedirects/RedirectsManager.php
+++ src/Bwebi/SeoRedirects/RedirectsManager.php
@@ -1,4 +1,6 @@
-<?php namespace Bwebi\SeoRedirects;
+<?php
+
+namespace Bwebi\SeoRedirects;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -10,6 +12,7 @@ class RedirectsManager
 
     /**
      * RedirectsManager constructor.
+     *
      * @param Request $request
      */
     public function __construct(Request $request)
@@ -20,23 +23,26 @@ class RedirectsManager
     /**
      * @return array|bool
      */
-    public function shouldBeRedirected() {
+    public function shouldBeRedirected()
+    {
         $result = $this->findRequest($this->request);
         if (!is_null($result)) {
             return ['redirect_to' => $result->to_url, 'status_code' => $this->getRedirectStatusCode($result->status_code)];
-        }
-        else {
+        } else {
             return false;
         }
     }
 
     /**
      * @param $request
+     *
      * @return mixed
      */
-    public function findRequest($request) {
+    public function findRequest($request)
+    {
         try {
             $this->redirect = DB::table('redirections')->where('from_url', $request->getRequestUri())->first();
+
             return $this->redirect;
         } catch (\Exception $ex) {
             Sentry::captureException($ex);
@@ -45,12 +51,15 @@ class RedirectsManager
 
     /**
      * @param $status_code
+     *
      * @return int
      */
-    public function getRedirectStatusCode($status_code) {
-        if (!is_null($status_code) && !empty($status_code))
+    public function getRedirectStatusCode($status_code)
+    {
+        if (!is_null($status_code) && !empty($status_code)) {
             return $status_code;
-        else
+        } else {
             return 301;
+        }
     }
 }
